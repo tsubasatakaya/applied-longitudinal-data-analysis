a738ac3fe46145b4451f3c5b14cf54fdf0df6c51
T.Takaya <117486701+tsubasatakaya@users.noreply.github.com>
2025-03-25 22:57:33 +0100

    modify plot size

--- research_paper/static_twfe.R
+++ research_paper/static_twfe.R
@@ -278,7 +278,7 @@ sta_twfe_duration_slope_plot <- partial_slope_data |>
         legend.text = element_text(size = 10))
 sta_twfe_duration_slope_plot
 ggsave(file.path(output_path, "static_twfe_duration_interation_plot.png"),
-       sta_twfe_duration_slope_plot, width = 7, height = 5, units = "in", dpi = 300)
+       sta_twfe_duration_slope_plot, width = 6, height = 6, units = "in", dpi = 300)
 
 
 
