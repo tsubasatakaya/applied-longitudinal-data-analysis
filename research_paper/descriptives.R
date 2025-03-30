62b0b3c2f9e84812334f0659ef29857d016892a0
T.Takaya <117486701+tsubasatakaya@users.noreply.github.com>
2025-03-30 14:28:44 +0200

    within variation WIP

--- research_paper/descriptives.R
+++ research_paper/descriptives.R
@@ -1,7 +1,7 @@
 source("research_paper/setup.R")
 source("research_paper/process_data.R")
 ################################################
-# Sample statistics
+# Sample statistics (on person-year level)
 ################################################
 table1::label(data_processed$sex) <- "Sex"
 table1::label(data_processed$age) <- "Age"
@@ -14,3 +14,25 @@ desc_tab <- table1(~ sex + age + edu + has_kid + emp + log_income
                    + depression | partnership,
                    data = data_processed)
 desc_tab
+
+print(paste0("Number of person years: ", nrow(data_processed)))
+print(paste0("Number of persons: ", length(unique(data_processed$id))))
+
+################################################
+# Within variation
+################################################
+variables <- c("age", "edu", "has_kid", "emp", "log_income", "depression")
+calculate_within_statistics <- function(x) {
+  
+}
+data_processed |> 
+  mutate(across(all_of(variables), ~ as.numeric(.x) - mean(as.numeric(.x), na.rm = TRUE),
+                .names = "{.col}_demeaned"),
+         .by = id) |> 
+  select(dplyr::ends_with("demeaned")) |> 
+  pivot_longer(dplyr::ends_with("demeaned"), names_to = "variable", values_to = "value") |> 
+  mutate(variable = str_replace(variable, "_demeaned", "")) |> 
+  group_by(variable) |> 
+  summarize(within_sd = sd(value),
+            within_min = min(value),
+            within_max = max(value))
